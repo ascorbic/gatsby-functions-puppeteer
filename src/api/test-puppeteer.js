9c7eba88932511ebf7504a308043c0d783bbe4dc
pauliescanlon <pauliescanlon@gmail.com>
2021-06-09 07:44:28 +0100

    feat: add chrome-aws-lambda

--- src/api/test-puppeteer.js
+++ src/api/test-puppeteer.js
@@ -1,7 +1,13 @@
-const puppeteer = require('puppeteer');
+const chromium = require('chrome-aws-lambda');
 
 export default async function handler(req, res) {
-  const browser = await puppeteer.launch({ headless: true });
+  const browser = await chromium.puppeteer.launch({
+    args: chromium.args,
+    defaultViewport: chromium.defaultViewport,
+    executablePath: await chromium.executablePath,
+    headless: chromium.headless,
+    ignoreHTTPSErrors: true
+  });
   const page = await browser.newPage();
 
   await page.goto('http://example.com/', { waitUntil: 'networkidle0' });
